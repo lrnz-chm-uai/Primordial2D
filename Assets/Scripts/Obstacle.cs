6ca29731f5b512ff29e2855d1b8604db8b7816c3
lrnz-chm-uai <lorenzo.christianimansilla@alumnos.uai.edu.ar>
2026-09-09 14:41:49 -0300

    First script to define prefab Obstacle behavior

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    Rigidbody2D rb;
+    public float minSpeed = 100f;
+    public float maxSpeed = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        transform.localScale = new Vector3(randomSize, randomSize, 44);
+
+        rb = GetComponent<Rigidbody2D>();
+        Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * randomSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
